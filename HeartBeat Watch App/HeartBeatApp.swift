3cca13154799cf728eedf285403a4f0040aa48cb
Andy Hollings <andy@arh.me.uk>
2024-10-03 08:59:44 +0100

    Initial Commit

---
+++ HeartBeat Watch App/HeartBeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartBeatApp.swift
+//  HeartBeat Watch App
+//
+//  Created by administrator on 03/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct HeartBeat_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HeartBeat Watch AppTests/HeartBeat_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HeartBeat_Watch_AppTests.swift
+//  HeartBeat Watch AppTests
+//
+//  Created by administrator on 03/10/2024.
+//
+
+import Testing
+@testable import HeartBeat_Watch_App
+
+struct HeartBeat_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
